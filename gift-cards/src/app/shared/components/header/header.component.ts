5851d1ffe27db1bc90459e8ee3b1549527ac17cd
Juddy Reina Pardo <tech.diidoo@gmail.com>
2025-02-12 19:10:56 -0500

    se agrega logout y header

--- gift-cards/src/app/shared/components/header/header.component.ts
+++ gift-cards/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
+import { AlertService } from '../../../core/services/alerts.service';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +10,23 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  title: string = '';
+
+  constructor(
+    private alertService: AlertService,
+    private authService: AuthService, 
+    private router: Router) {
+      this.title = this.router.url.replace('/', '');
+    }
+
+    async logout() {
+      try {
+        await this.authService.signOut();
+      }catch (error) {
+        this.alertService.error('Tuvimos un problema, vuelve a intentar más tarde.');
+
+      }
+    }
 
 }
