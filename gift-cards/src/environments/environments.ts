606647e563a28dcea36bea260a701e3bec8348ae
Juddy Reina Pardo <tech.diidoo@gmail.com>
2025-02-13 15:18:02 -0500

    se corrige problema con environments por el deploy a github pages

---
+++ gift-cards/src/environments/environments.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    apiUrl: '', //endpoint del backend
+    userPoolId: '',//id del grupo de usuario de Cognito
+    userPoolClientId: '',//id del cliente de aplicación relacionado con el grupo de usuario de Cognito
+    region: ''//region del grupo de usuario de Cognito
+  };
